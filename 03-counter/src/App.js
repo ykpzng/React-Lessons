e514fc8a399b6d75848699fedaf758df08814834
Yakup ZENGİN <48925129+ykpzng@users.noreply.github.com>
2020-12-20 12:55:26 +0300

    Add 03-counter

---
+++ 03-counter/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./Counter";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <br></br>
+      <hr />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03-counter/src/Footer.js
@@ -0,0 +1,25 @@
+//*Class component - State
+
+//import React, { Component } from "react";
+
+// export default class Footer extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>FOOTER FIELD</h1>
+//       </div>
+//     );
+//   }
+// }
+
+//* Functional Component- Stateless Component
+
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <h1>FOOTER FIELD</h1>
+    </div>
+  );
+}
